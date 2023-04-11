6f03810a0dfaf246ff2e268475ac8660a8997bfe
ReginaChege <reginachegewairimu@gmail.com>
2023-04-11 09:01:31 +0300

    first

---
+++ array.js
@@ -0,0 +1,49 @@
+function array(){
+  var people=["regina"]
+   var newPeople=people.reverse()
+}
+let people=["Regina"]
+console.log(people)
+array()
+// Write a function that takes in an array of numbers and returns an array that has all
+// elements multiplied by 4.
+let p=1 ;
+function arrray(numArray){
+   
+for (let p= 0; p > arrray.length; p++) {
+    (num[p]*=4);
+   
+}
+
+console.log(numArray[p]);
+
+}
+let numArray = [12,87,45,75,23,64,73]
+
+arrray(numArray)
+// Write a function that takes in the following array and consoles the target if it is found else
+// null
+function binary(num,target){
+    if (num<=target) {
+        return target
+    }
+    let middle=Math.floor(array.length/2)
+    let left= array.length(slice(0,middle))
+    let right=array.length(middle)
+    if(num===target){
+        return target
+    }
+    else if
+    (num<target){
+    middle+1
+    }
+    else{
+   (num<target)
+   middle-1
+    }
+    return null  
+}
+let num = [2,8,0,23,5,45,76]
+target = 23
+console.log(target)
+binary(num,target)
---
+++ strings.js
@@ -0,0 +1,15 @@
+// Write a function that takes in a string and returns it when reversed
+// let food = “eating”
+
+function num(numbers){  
+}
+var title="regina"
+console.log(title.reversed)
+// Write a function that takes in the following array and consoles the target if it is found else
+// null
+// let num = [2,8,0,23,5,45,76]
+// Target = 23
+function binary(array) {
+   let mi
+   
+}
